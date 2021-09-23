c420758ae88eae0c4409ff86233abbdd5488f2b7
Nozidoali <nozidoali@gmail.com>
2021-09-23 16:52:30 +0200

    update(course): add all the courses to the homepage

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,11 @@
 $(function(){
-    $("#nav div").click(function(){
-        $("#display div").eq($("#nav div").index(this)).show().siblings("#display div").hide();
+    $("#output_nav div").click(function(){
+        $("#output_display div").eq($("#output_nav div").index(this)).show().siblings("#output_display div").hide();
+    });
+});
+
+$(function(){
+    $("#input_nav div").click(function(){
+        $("#input_display div").eq($("#input_nav div").index(this)).show().siblings("#input_display div").hide();
     });
 });
